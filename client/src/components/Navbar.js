63e18afe933eb8271abd4fccc96d44367ef9da8c
ncmarsh <68823943+ncmarsh@users.noreply.github.com>
2021-02-07 17:13:26 -0800

    set up navbar

---
+++ client/src/components/Navbar.js
@@ -0,0 +1,35 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+function Navbar() {
+    return (
+        <div>
+            <nav>
+                <ul className="navbar navbar-expand-lg navbar-light bg-light">
+                    <a className="navbar-brand" href="#">
+                        Google Books
+                    </a>
+                    <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+                        <div className="navbar-nav">
+                            <li className="nav-item">
+                                <Link to="/search" className="nav-link">
+                                    Search
+                                </Link>
+                            </li>
+                            <li className="nav-item">
+                                <Link to="/saved" className="nav-link">
+                                    Saved
+                                </Link>
+                            </li>
+                        </div>
+                    </div>
+                </ul>
+            </nav>
+        </div>
+    )
+}
+
+export default Navbar;
