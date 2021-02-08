91a10365fbc82d1dfa45d84217919bfab841cdc6
ncmarsh <68823943+ncmarsh@users.noreply.github.com>
2021-02-07 17:14:31 -0800

    set up grid components

---
+++ client/src/components/Grid.js
@@ -0,0 +1,32 @@
+import React from "react";
+
+// Container
+export function Container ({ children }) {
+    return (
+        <div className={`container${fluid ? "-fluid" : ""}`}>
+            {children}
+        </div>
+    )
+}
+
+// Row
+export function Row({ fluid, children }) {
+    return (
+        <div className={`row${fluid ? "-fluid" : ""}`}>
+            {children}
+        </div>
+    )
+}
+
+// Col
+export function Col({ size, children }) {
+    return (
+        <div className={size
+            .split(" ")
+            .map(size => "col-" + size)
+            .join(" ")}
+        >
+            {children}
+        </div>
+    )
+}
