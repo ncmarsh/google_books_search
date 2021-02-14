3eb300bea6e7368f4dd690c5f4dd11a805dfda83
ncmarsh <68823943+ncmarsh@users.noreply.github.com>
2021-02-14 15:21:52 -0800

    updated navbar app name href

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -6,7 +6,7 @@ function Navbar() {
         <div>
             <nav>
                 <ul className="navbar navbar-expand-lg navbar-light bg-light">
-                    <a className="navbar-brand" href="#">
+                    <a className="navbar-brand" href="/">
                         Google Books
                     </a>
                     <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
