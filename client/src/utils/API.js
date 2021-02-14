3e5440b627c0fb844c142fe7a235987e9d340e06
ncmarsh <68823943+ncmarsh@users.noreply.github.com>
2021-02-14 15:25:14 -0800

    began api calls

---
+++ client/src/utils/API.js
@@ -0,0 +1,20 @@
+import axios from "axios";
+const BASEURL = "https://www.googleapis.com/books/v1/volumes?q="
+
+export default {
+    googleBooksSearch: function(query) {
+        return axios.get(BASEURL + query)
+    },
+    getBooks: function() {
+        return axios.get("/api/books");
+    },
+    getBook: function(id) {
+        return axios.get("api/books/" + id);
+    },
+    deleteBook: function(id) {
+        return axios.delete("api/books/" + id);
+    },
+    saveBook: function(bookData) {
+        return axios.post("/api/books/", bookData);
+    }
+};
