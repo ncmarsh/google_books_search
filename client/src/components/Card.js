329f503cd5c9ab8b001942646eba9d37de00df2a
ncmarsh <68823943+ncmarsh@users.noreply.github.com>
2021-02-14 15:23:21 -0800

    added card component

---
+++ client/src/components/Card.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+const styles = {
+    card: {
+        height: "auto",
+        padding: "30px",
+        backgroundColor: "#dddddd",
+        marginBottom: "20px"
+    }
+}
+
+function Card(props) {
+    return (
+        <div {...props} className="card" style={styles.card}>
+            {props.children}
+        </div>
+    )
+}
+
+export default Card;
