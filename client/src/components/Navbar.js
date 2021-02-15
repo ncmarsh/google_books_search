ee540e4ad976a97fe3835d3461dd164303238ad9
ncmarsh <68823943+ncmarsh@users.noreply.github.com>
2021-02-14 18:23:32 -0800

    updated navbar

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -3,32 +3,30 @@ import { Link } from "react-router-dom";
 
 function Navbar() {
     return (
-        <div>
-            <nav>
-                <ul className="navbar navbar-expand-lg navbar-light bg-light">
-                    <a className="navbar-brand" href="/">
-                        Google Books
-                    </a>
-                    <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-                        <span className="navbar-toggler-icon"></span>
-                    </button>
-                    <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
-                        <div className="navbar-nav">
-                            <li className="nav-item">
-                                <Link to="/search" className="nav-link">
-                                    Search
-                                </Link>
-                            </li>
-                            <li className="nav-item">
-                                <Link to="/saved" className="nav-link">
-                                    Saved
-                                </Link>
-                            </li>
-                        </div>
+        <nav className="fixed-top sticky-top">
+            <ul className="navbar fixed-top sticky-top navbar-expand-lg navbar-light bg-light">
+                <a className="navbar-brand" href="/">
+                    Google Books
+                </a>
+                <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+                    <div className="navbar-nav">
+                        <li className="nav-item">
+                            <Link to="/search" className="nav-link">
+                                Search
+                            </Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link to="/saved" className="nav-link">
+                                Saved
+                            </Link>
+                        </li>
                     </div>
-                </ul>
-            </nav>
-        </div>
+                </div>
+            </ul>
+        </nav>
     )
 }
 
