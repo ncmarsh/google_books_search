9bc90b1a007a139b0163adfe063a4e837b05eb34
ncmarsh <68823943+ncmarsh@users.noreply.github.com>
2021-02-14 18:20:54 -0800

    updated styling on bookcard

--- client/src/components/BookCard.js
+++ client/src/components/BookCard.js
@@ -1,25 +1,37 @@
 import React from "react";
 
 const styles = {
+    bookCard:{
+        border: "3px #000000 solid",
+        marginBottom: "8px",
+        padding: "10px"
+    },
     image: {
-        height: "50px",
-        width: "50px",
-        float: "left"
+        float: "left",
+        width: "100px",
+        height: "auto",
+        marginRight: "0.5rem"
+    },
+    text: {
+        display: "inline-block"
     }
 }
 
 function BookCard(props) {
     return (
-        <div className="card">
+        <div className="card" style={styles.bookCard}>
             <h3>
                 <a href={props.link}>
                     {props.title}
                 </a> 
             </h3>
             <h4>{props.subtitle}</h4>
-            <h5>{props.author}</h5>
-            <img src={props.image} alt="Cover Image" style={styles.image} />
-            <p>{props.description}</p>
+            <h5>Written by: {props.author}</h5>
+            <hr />
+            <div style={styles.text}>
+                <img src={props.image} alt="Cover Image" className="img-fluid" style={styles.image} />
+                <p>{props.description}</p>
+            </div>
         </div>
     );
 };
