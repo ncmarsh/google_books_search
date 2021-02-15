1bdfdac7ef4a269912567df4ad6c288118a29b3f
ncmarsh <68823943+ncmarsh@users.noreply.github.com>
2021-02-15 15:51:37 -0800

    updating API calls

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -9,14 +9,14 @@ export default {
         console.log(bookData);
         return axios.post("/api/books/", bookData);
     },
-
-    // getBooks: function() {
-    //     return axios.get("/api/books");
-    // },
-    // getBook: function(id) {
-    //     return axios.get("api/books/" + id);
-    // },
-    // deleteBook: function(id) {
-    //     return axios.delete("api/books/" + id);
-    // }
+    getBooks: function() {
+        return axios.get("/api/books");
+    },
+    
+    getBook: function(id) {
+        return axios.get("api/books/" + id);
+    },
+    deleteBook: function(id) {
+        return axios.delete("api/books/" + id);
+    }
 };
